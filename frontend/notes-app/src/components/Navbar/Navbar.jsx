ac63cd25ca98bd9fb1a28c6bf04140b55bd1a8bc
Subham <mr.subhamdas999@gmail.com>
2024-09-03 22:15:20 +0530

    first Commit

---
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
+import SearchBar from '../SearchBar/searchBar'
+
+const Navbar = () => {
+  const [searchQuery, setSearchQuery]=useState("");
+  const navigate =useNavigate
+  const onLogout =()=> {
+    navigate("/login")
+  };
+
+  const handleSearch=()=>{
+
+  };
+  const onClearSearch=()=>{
+    setSearchQuery("");
+  }
+
+  return (
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow-xl'>
+        <h2 className='test-xl font-medium text-black py-2'>DIGINotes</h2>
+        <SearchBar 
+          value={searchQuery}
+          onChange={({target})=>{
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+        <ProfileInfo onLogout={onLogout}/>
+    </div>
+  )
+}
+
+export default Navbar
